b378d5ff05d09fa8ec3bae12cd16bb26c2970076
Joseph Aje <ajejoseph22@gmail.com>
2021-01-27 14:44:36 +0100

    added morgan, cors and body parsing middleware

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import logger from "winston";
+import morgan from "morgan";
+import cors from "cors";
 import introduction from "./controllers/introduction";
 import validate from "./routes/validate";
 
 const app = express();
 
-app.use("/", validate);
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(json());
+app.use(urlencoded());
 
 // ROUTES
 app.get("/", introduction);
+app.use("/validate", validate);
 
 app.listen(3000, () => {
   logger.info("App successfully started");
